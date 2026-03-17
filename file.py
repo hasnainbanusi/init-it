347ef14b1a496b6536e5bf1d4ff431d2247b3459
Hasnain Ahmad Khan <hasnain10.official@gmail.com>
2026-03-18 01:25:45 +0500

    Add files via upload

---
+++ file.py
@@ -0,0 +1,11 @@
+print("Hello world")
+
+Github Commands
+1. git init (initilization)
+2. git status (check status of file)
+3. git add file-name (add file to stage from untracked status)
+4. git commit -m file-name (to commit file into tracked status)
+5. git restore file-name (to recover deleted file)
+6. git remote add origin (Paste link here)
+7. git remote -v (to check github repo)
+8. git push -u origin master (to push file to github repo)
